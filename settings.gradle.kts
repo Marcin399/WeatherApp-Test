76bb9866f51df1880f673f0449f1ccc5f20eafa7
Marcin Janiczek <inspiremarcin@gmail.com>
2022-08-15 21:26:04 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Weather_app_Test"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/weatherapptest/Greeting.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapptest
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
